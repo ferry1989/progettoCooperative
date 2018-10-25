db1ed40e01538aafb62c122bfd651ff9f3e0f951
Jacopo Ferrara <jferrara@alpenite.com>
2018-10-25 16:36:15 +0200

    no message

--- script/login.php
+++ script/login.php
@@ -6,15 +6,17 @@ $msg = "";
 	if($con != null){
 		$username = mysqli_real_escape_string($con, $_POST['username']);
 		$password = mysqli_real_escape_string($con, $_POST['password']);
-		$verificaUtente = "SELECT isAdmin,user FROM utente where user = '$username' and password = '$password'";
+		$verificaUtente = "SELECT isAdmin,user,id_regione,id_ente FROM utente where user = '$username' and password = '$password'";
 		$result = mysqli_query($con,$verificaUtente);
 
 		if ($row = $result->fetch_assoc()) {
 			session_start();
 			$_SESSION['isAdmin'] = $row["isAdmin"];
+			$_SESSION['id_regione'] = $row["id_regione"];
+			$_SESSION['id_ente'] = $row["id_ente"];
 			$_SESSION['username'] = trim($row["user"]);
 			$msg = array("success"=>"Login Effettuato");
-			header("location: ../index.php");
+			header("location: ../site/dashboard.php");
 		}
 		else{
 			$msg = array("error"=>"Combinazione username/password errata!");
---
+++ site/include/menu.php
@@ -0,0 +1,40 @@
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="indexr.html">
+                	<img src="imgCustom/logo.jpg" style="height:26px; width: inherit;">
+                </a>
+            </div>
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="dashboard.php"><i class="fa fa-dashboard fa-fw"></i>Pannello di Controllo</a>
+                        </li>
+						 <li>
+                            <a href="#"><i class="fa fa-user fa-fw"></i><span data-toggle="modal" data-target="#inserisciente">Inserisci Ente</span></a>
+                        </li>
+						<li>
+                            <a href="indexrcercaente.php"><i class="fa fa-user fa-fw"></i>Ricerca Ente</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-th-list fa-fw"></i> <span data-toggle="modal" data-target="#inserisciprogetto">Inserisci Progetto</i></a>
+                        </li>
+						<li>
+                            <a href="indexrcercaprogetto.php"><i class="fa fa-th-list fa-fw"></i>Ricerca Progetto</a>
+                        </li>
+                        
+                        <li>
+                            <a href="indexrcercavolontario.php"><i class="fa fa-user fa-fw"></i>Gestione Volontari</a>
+                        </li>
+						
+                    </ul>
+                </div>
+            </div>
+        </nav>
